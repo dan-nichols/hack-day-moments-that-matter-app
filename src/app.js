d6d5e138666b3aae137b37e4049635db128e9457
CORBETT, David <david.corbett@suncorp.com.au>
2017-05-11 14:42:13 +1000

    [TGB-3495] - David C/Hari - introduced the ?Home screen

--- src/app.js
+++ src/app.js
@@ -1,40 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
+    AppRegistry
 } from 'react-native';
+import Home from './screen/home';
 
-export default class FinancialFitnessApp extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to Financial Fitness App!
-                </Text>
-            </View>
-    );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
-
-AppRegistry.registerComponent('FinancialFitnessApp', () => FinancialFitnessApp);
+AppRegistry.registerComponent('FinancialFitnessApp', () => Home);
