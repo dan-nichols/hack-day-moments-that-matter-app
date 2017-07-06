2df036e7b2f247b28aeceb9368f15ce118dc8fac
NICHOLS, Daniel <Daniel.NICHOLS@suncorp.com.au>
2017-07-06 14:47:03 +1000

    Merging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
-    AppRegistry
+    AppRegistry,
+    StyleSheet,
+    View
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Home from './components/screen/home';
@@ -10,9 +12,24 @@ import Screen2 from './components/screen/screen2';
 export default class App extends Component {
     render () {
         return (
-            <Home />
+            <View>
+                <View style={styles.toolbar}/>
+                <Home />
+            </View>
         );
     }
 }
 
 AppRegistry.registerComponent('FinancialFitnessApp', () => App);
+
+
+const styles = StyleSheet.create({
+    toolbar: {
+        backgroundColor: '#808080',
+        height: 23
+    },
+    page: {
+        backgroundColor: '#1f1f1f',
+        flex: 1
+    }
+});
--- src/components/screen/home.js
+++ src/components/screen/home.js
@@ -1,64 +1,35 @@
 import React, {Component} from 'react';
+
+import { StyleSheet, Dimensions } from 'react-native';
 import {
     Button,
-    Dimensions,
-    View,
-    Alert
-} from 'react-native';
-import { DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
+    DeckSwiper,
+    Card,
+    CardItem,
+    Thumbnail,
+    Text,
+    Left,
+    Body,
+    Icon } from 'native-base';
 
-import styles from './styles';
 import CarSummaryScreen from "./carSummaryScreen";
-
-
-
-// test data
-const DATA = [
-  {
-    id: 1,
-    text: 'Honda Civic',
-    age: 1990,
-    img: require('../../img/civic.jpg'),
-    carRego : {
-        plateNumber:'AUS000',
-        daysLeft:132
-    },
-    carInsurance : {
-      insurer:'SUNCORP',
-      typeOfCover:'Third Party',
-      repaymentCycle:'Yearly',
-      daysLeft:3,
-      insurerImage:require('../../img/aami-logo.jpg')
-    }
-  },
-  {
-    id: 2,
-    text: 'Subaru Impreza',
-    age: 2002,
-    img: require('../../img/impreza.jpg'),
-    carRego : {
-        plateNumber:'USA911',
-        daysLeft:7
-    },
-    carInsurance : {
-        insurer:'AAMI',
-        typeOfCover:'Comprehensive',
-        repaymentCycle:'Monthly',
-        daysLeft:27,
-        insurerImage:require('../../img/suncorp-logo.jpg')
-    }
-  },
-];
 
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height
 
+// test data
+const DATA = [
+  { id: 1, text: 'Honda Civic', age: 1990, img: require('../../img/civic.jpg') },
+  { id: 2, text: 'Subaru Impreza', age: 2002, img: require('../../img/impreza.jpg') },
+  { id: 0, text: 'Subaru Impreza', age: 2002, img: require('../../img/impreza.jpg') }
+];
+
 
 class Home extends Component {
 
 
-    constructor({navigation}) {
+    constructor() {
         super()
     }
 
@@ -68,16 +39,56 @@ class Home extends Component {
             <DeckSwiper
                 dataSource={DATA}
                 renderItem={item =>
-              <Card style={{ elevation: 3 }}>
-                <CarSummaryScreen item={item}/>
-              </Card>
-            }
+                    item.id === 0 ?
+                        <Card style={styles.card}>
+                            <CardItem header>
+                                <Text>What matters to you?</Text>
+                            </CardItem>
+
+                            <CardItem style={styles.cardItem}>
+                                <Button style={styles.button} block>
+                                    <Text>New Car</Text>
+                                </Button>
+                            </CardItem>
+
+                            <CardItem style={styles.cardItem}>
+                                <Button style={styles.button} block>
+                                    <Text>New House</Text>
+                                </Button>
+                            </CardItem>
+
+                            <CardItem style={styles.cardItem}>
+                                <Button style={styles.button} block>
+                                    <Text>New Business</Text>
+                                </Button>
+                            </CardItem>
+                        </Card> :
+
+                        <Card style={styles.card}>
+                            <CarSummaryScreen item={item}/>
+                        </Card>
+                }
           />
 
         );
     }
 
 }
+
+
+const styles = {
+    card: {
+        height: SCREEN_HEIGHT - 30,
+        elevation: 3
+    },
+    button: {
+        flex: 1
+    },
+    cardItem: {
+        flexDirection:'row'
+    }
+};
+
 
 export default Home;
 
